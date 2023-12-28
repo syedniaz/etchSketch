a64577ed8dd8942ff8107fed200aacf5843c46ed
syedniaz <snm051110@gmail.com>
2023-12-28 20:39:24 +0600

    added styles

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let color = 'black';
+let click = 'false';
 
 function populateBoard(size){
     let board = document.querySelector('.board');
@@ -20,20 +21,22 @@ populateBoard(16);
 
 function changeSize(input){
     if (input >= 2 && input <= 100){
+        document.querySelector(".error").style.display = "none";
         populateBoard(input);
     }
     else {
-        console.log('too many squares');
+        document.querySelector(".error").style.display = "flex";
     }
 }
 
 function colorSquare(){
-    if (color === 'random'){
-        // random color
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    }
-    else {
-        this.style.backgroundColor = color;
+    if (!click){
+        if (color === 'random'){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        }
+        else {
+            this.style.backgroundColor = color;
+        }
     }
 }
 
@@ -46,3 +49,15 @@ function clearBoard(){
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.style.backgroundColor = 'white');
 }
+
+document.querySelector('body').addEventListener('click', (e) =>{
+    if (e.target.tagName != 'BUTTON'){
+        click = !click;
+        if (!click){
+            document.querySelector(".mode").textContent = "Mode: Coloring";
+        }
+        else {
+            document.querySelector(".mode").textContent = "Mode: Not Coloring";
+        }
+    }
+});
